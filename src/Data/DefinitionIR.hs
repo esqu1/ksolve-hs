55cfc0275a822a3af17da4c398f7cfd53de7172c
Brandon Lin <brandbest1@gmail.com>
2020-06-04 02:13:18 -0400

    format, add more parsers

--- src/Data/DefinitionIR.hs
+++ src/Data/DefinitionIR.hs
@@ -1,16 +1,65 @@
-module Data.DefinitionIR where 
+module Data.DefinitionIR where
 
-data CubieProperty = Permutation | Orientation
-data Set = Set {name:: String, positions:: Int, orientations:: Int, solved:: Position}
-data Position = Position {permutation :: [Int], orientation :: [Int]}
-data PositionMask = PositionMask {permMask :: [Bool], orientMask:: [Bool]}
-data SetPosition = SetPosition {setName:: String, moveEffect :: Position}
+data CubieProperty = Permutation
+    | Orientation
+
+data Set = Set
+    { name         :: String
+    , positions    :: Int
+    , orientations :: Int
+    , solved       :: Position
+    }
+
+data Position = Position
+    { permutation :: [Int]
+    , orientation :: Maybe [Int]
+    }
+
+data PositionMask = PositionMask
+    { permMask   :: [Bool]
+    , orientMask :: [Bool]
+    }
+
+data SetPosition = SetPosition
+    { setName    :: String
+    , moveEffect :: Position
+    }
+
 type SolvedState = [SetPosition]
+
 type MoveEffect = [SetPosition]
+
 data MoveName = String
-data Move = Move {moveName :: MoveName, effect :: MoveEffect}
-data MoveInstance = MoveInstance {baseMove:: Move, rotationCount:: Int, prime:: Bool}
-data Ignore = Ignore {ignoredSetName :: String, ignoredFacelets :: PositionMask}
-data MoveCombination = MoveCombination {resultMove:: MoveName, components :: [MoveName]}
-data PruningTable = PruningTable {prunedSetName :: String, property:: CubieProperty, maxStates:: Int}
-type PruningTables = [PruningTable]
+
+data Move = Move
+    { moveName :: MoveName
+    , effect   :: MoveEffect
+    }
+
+data MoveInstance = MoveInstance
+    { baseMove      :: Move
+    , rotationCount :: Int
+    , prime         :: Bool
+    }
+
+data Ignore = Ignore
+    { ignoredSetName  :: String
+    , ignoredFacelets :: PositionMask
+    }
+
+data MoveCombination = MoveCombination
+    { resultMove :: MoveName
+    , components :: [MoveName]
+    }
+
+data PruningTable = PruningTable
+    { prunedSetName :: String
+    , property      :: CubieProperty
+    , maxStates     :: Int
+    }
+
+data Section = PruningTables [PruningTable]
+    | Solved SolvedState
+    | SetDef Set
+    | MoveDef Move
+    | MoveCombos [MoveCombination]
--- src/DefParser.hs
+++ src/DefParser.hs
@@ -1,7 +1,9 @@
 module DefParser where
-import Data.Char
-import Text.ParserCombinators.ReadP
-import Data.DefinitionIR
+
+import           Data.Char
+import           Data.DefinitionIR
+import           Data.Maybe
+import           Text.ParserCombinators.ReadP
 
 parseInt :: ReadP Int
 parseInt = read <$> many (satisfy isDigit)
@@ -10,13 +12,28 @@ parseSetName :: ReadP String
 parseSetName = many (satisfy isAlpha)
 
 parseName :: ReadP [Char]
-parseName = string "Name" *> skipSpaces *> many1(satisfy (\x -> (x /= ' ')))
+parseName = string "Name" *> skipSpaces *> many1 (satisfy (\x -> (x /= ' ')))
 
 parseSetDef :: ReadP Set
-parseSetDef = Set <$> (string "Set" *> parseSetName) <*> parseInt <*> parseInt <*> return (Position [] [])
+parseSetDef =
+  Set <$> (string "Set" *> parseSetName) <*> parseInt <*> parseInt <*>
+  return (Position [] (Just []))
 
-parseAllSetDefs :: ReadP Set
+parseAllSetDefs :: ReadP [Set]
 parseAllSetDefs = sepBy parseSetDef skipSpaces
 
+parseSetPosition :: ReadP SetPosition
+parseSetPosition =
+  (\x y z -> SetPosition x (Position y z)) <$> parseSetName <*>
+  (skipSpaces *> endBy1 parseInt skipSpaces) <*>
+  option Nothing (Just <$> endBy1 parseInt skipSpaces)
+
 parseSolvedState :: ReadP SolvedState
-parseSolvedState = string "Solved\n" *> 
+parseSolvedState =
+  skipSpaces *> string "Solved" *> skipSpaces *>
+  sepBy1 parseSetPosition skipSpaces <*
+  string "End" <*
+  skipSpaces
+
+parseSection :: ReadP Section
+parseSection = undefined
